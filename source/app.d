a551b30e5720c21a777dacff8840cebf9245cf1b
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2022-11-08 10:19:25 +0200

    Added error handling when doing any JSON parsing such that errors will be sent to the response on Gitea

--- source/app.d
+++ source/app.d
@@ -12,94 +12,146 @@ private class IRCBot : Client
 	{
 		super(connInfo);
 	}
-}
 
-void pullRequestHandler(HTTPServerRequest request, HTTPServerResponse response)
-{
-
+	public override void onChannelMessage(Message msg, string, string)
+	{
+		// this.sendMessage("fok", "#tlang"BB);
+		// channelMessage("fok", "#tlang");
+	}
 }
 
 void commitHandler(HTTPServerRequest request, HTTPServerResponse response)
 {
-	//TODO: Add error handling for json parsing
-	JSONValue json = parseJSON(request.json().toString());
+	/* Reply data */
+	JSONValue replyJSON;
+	int replyCode = 200;
+	replyJSON["output"]="";
 
-	writeln(json.toPrettyString());
+	try
+	{
+		//TODO: Add error handling for json parsing
+		JSONValue json = parseJSON(request.json().toString());
 
-	// Extract the commit
-	JSONValue commitBlock = json["commits"].array()[0];
-	string commitMessage = strip(commitBlock["message"].str());
-	string commitURL = commitBlock["url"].str();
-	string commitID = commitBlock["id"].str();
+		writeln(json.toPrettyString());
 
-	JSONValue authorBlock = commitBlock["author"];
-	string authorName = authorBlock["name"].str();
-	string authorEmail = authorBlock["email"].str();
+		// Extract the commit
+		JSONValue commitBlock = json["commits"].array()[0];
+		string commitMessage = strip(commitBlock["message"].str());
+		string commitURL = commitBlock["url"].str();
+		string commitID = commitBlock["id"].str();
 
-	response.writeBody("Yo");
+		JSONValue authorBlock = commitBlock["author"];
+		string authorName = authorBlock["name"].str();
+		string authorEmail = authorBlock["email"].str();
 
-	string ircMessage = "Commit: "~commitMessage~" ("~commitID~") by "~authorName~" ("~authorEmail~") ["~commitURL~"]";
-	ircBot.channelMessage(ircMessage, "#tlang");
+		//FIXME: Reply with 200 or 500 on error
+		
+		string ircMessage = "Commit: "~commitMessage~" ("~commitID~") by "~authorName~" ("~authorEmail~") ["~commitURL~"]";
+		ircBot.channelMessage(ircMessage, "#tlang");
+	}
+	catch(Exception e)
+	{
+		replyCode=500;
+		replyJSON["output"]=e.toString();
+	}
+	
+	response.writeJsonBody(replyJSON, 200);
 }
 
 void issueHandler(HTTPServerRequest request, HTTPServerResponse response)
 {
-	//TODO: Add error handling for json parsing
-	JSONValue json = parseJSON(request.json().toString());
+	/* Reply data */
+	JSONValue replyJSON;
+	int replyCode = 200;
+	replyJSON["output"]="";
 
-	writeln(json.toPrettyString());
-
-	//Extract the type of action
-	JSONValue issueBlock = json["issue"];
-	string issueTitle = issueBlock["title"].str();
-	string issueURL = issueBlock["url"].str();
-	long issueID = issueBlock["id"].integer();
-	string issueAction = json["action"].str();
-
-	/* Opened a new issue */
-	if(cmp(issueAction, "opened") == 0)
+	try
 	{
-		JSONValue userBlock = issueBlock["user"];
-		string username = userBlock["username"].str();
-		
-		ircBot.channelMessage("Opened issue '"~issueTitle~"' (#"~to!(string)(issueID)~") by "~username~" ["~issueURL~"]", "#tlang");
-	}
-	/* Closed an old issue */
-	else if(cmp(issueAction, "closed") == 0)
-	{
-		JSONValue userBlock = issueBlock["user"];
-		string username = userBlock["username"].str();
-		
-		ircBot.channelMessage("Closed issue '"~issueTitle~"' (#"~to!(string)(issueID)~") by "~username~" ["~issueURL~"]", "#tlang");
-	}
-	/* Reopened an old issue */
-	else if(cmp(issueAction, "reopened") == 0)
-	{
-		JSONValue userBlock = issueBlock["user"];
-		string username = userBlock["username"].str();
-		
-		ircBot.channelMessage("Reopened issue '"~issueTitle~"' (#"~to!(string)(issueID)~") by "~username~" ["~issueURL~"]", "#tlang");
-	}
-	/* Added a comment */
-	else if(cmp(issueAction, "created") == 0)
-	{
-		JSONValue commentBlock = json["comment"];
-		string commentBody = commentBlock["body"].str();
+		//TODO: Add error handling for json parsing
+		JSONValue json = parseJSON(request.json().toString());
 
-		ulong commentLen = commentBody.length;
+		writeln(json.toPrettyString());
 
-		if(!(commentLen <= 30))
+		//Extract the type of action
+		JSONValue issueBlock = json["issue"];
+		string issueTitle = issueBlock["title"].str();
+		string issueURL = issueBlock["url"].str();
+		long issueID = issueBlock["id"].integer();
+		string issueAction = json["action"].str();
+
+		/* Opened a new issue */
+		if(cmp(issueAction, "opened") == 0)
 		{
-			commentBody=commentBody[0..31]~"...";
+			JSONValue userBlock = issueBlock["user"];
+			string username = userBlock["username"].str();
+			
+			ircBot.channelMessage("Opened issue '"~issueTitle~"' (#"~to!(string)(issueID)~") by "~username~" ["~issueURL~"]", "#tlang");
 		}
+		/* Closed an old issue */
+		else if(cmp(issueAction, "closed") == 0)
+		{
+			JSONValue userBlock = issueBlock["user"];
+			string username = userBlock["username"].str();
+			
+			ircBot.channelMessage("Closed issue '"~issueTitle~"' (#"~to!(string)(issueID)~") by "~username~" ["~issueURL~"]", "#tlang");
+		}
+		/* Reopened an old issue */
+		else if(cmp(issueAction, "reopened") == 0)
+		{
+			JSONValue userBlock = issueBlock["user"];
+			string username = userBlock["username"].str();
+			
+			ircBot.channelMessage("Reopened issue '"~issueTitle~"' (#"~to!(string)(issueID)~") by "~username~" ["~issueURL~"]", "#tlang");
+		}
+		/* Added a comment */
+		else if(cmp(issueAction, "created") == 0)
+		{
+			JSONValue commentBlock = json["comment"];
+			string commentBody = commentBlock["body"].str();
 
-		JSONValue userBlock = commentBlock["user"];
-		string username = userBlock["username"].str();
+			ulong commentLen = commentBody.length;
 
-		ircBot.channelMessage("New comment '"~commentBody~"' by "~username~" on issue #"~to!(string)(issueID)~" ["~issueURL~"]", "#tlang");		
+			if(!(commentLen <= 30))
+			{
+				commentBody=commentBody[0..31]~"...";
+			}
+
+			JSONValue userBlock = commentBlock["user"];
+			string username = userBlock["username"].str();
+
+			ircBot.channelMessage("New comment '"~commentBody~"' by "~username~" on issue #"~to!(string)(issueID)~" ["~issueURL~"]", "#tlang");		
+		}
 	}
+	catch(Exception e)
+	{
+		replyCode=500;
+		replyJSON["output"]=e.toString();
+	}
+	
+	response.writeJsonBody(replyJSON, 200);
+}
 
-	response.writeBody("Yo");
+void pullRequestHandler(HTTPServerRequest request, HTTPServerResponse response)
+{
+	/* Reply data */
+	JSONValue replyJSON;
+	int replyCode = 200;
+	replyJSON["output"]="";
+
+	try
+	{
+		//TODO: Add error handling for json parsing
+		JSONValue json = parseJSON(request.json().toString());
+
+		writeln(json.toPrettyString());
+	}
+	catch(Exception e)
+	{
+		replyCode=500;
+		replyJSON["output"]=e.toString();
+	}
+	
+	response.writeJsonBody(replyJSON, 200);
 }
 
 IRCBot ircBot;
@@ -129,14 +181,13 @@ void main()
 	httpServerSettings.port = 6969;
 	httpServerSettings.bindAddresses = ["::"];
 
+	/* Create a router and add the supported routes */
 	URLRouter router = new URLRouter();
-
-
 	router.post("/commit", &commitHandler);
 	router.post("/issue", &issueHandler);
 	router.post("/pullrequest", &pullRequestHandler);
 
-
+	/* Attach the router to the HTTP server settings */
 	listenHTTP(httpServerSettings, router);
 
 	/* Starts the vibe-d event engine web server on the main thread */
