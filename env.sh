fbb7192a366a2876ceecb4ae8a275175bcff032e
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-06-30 12:41:01 +0200

    Repo
    - Added basic environment variable usage

---
+++ env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Webhook configuration
+export WEBHOOK__BINDADDRESS="0.0.0.0"
+export WEBHOOK__PORT=8080
+
+# IRC server
+export IRC__HOST="pinewood.irc.bnet.eu.org"
+export IRC__PORT=6667
+export IRC__NICKNAME="GiteaBot"
+export IRC__REALNAME="A Gitea bot written by deavmi"
+export IRC__CHANNELS="tlang:#tlang;thing2:#thing2Chan"
